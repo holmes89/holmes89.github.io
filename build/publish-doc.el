421ff8c95d3fb871f937228fd60b51a50674a290
Joel Holmes <holmes89@gmail.com>
2021-05-07 13:27:43 -0400

    css

--- build/publish-doc.el
+++ build/publish-doc.el
@@ -4,7 +4,7 @@
 (require 'ox-html)
 
 (setq org-publish-project-alist
-      `(("wiki"
+      `(("wiki-content"
          :base-directory "."
          :base-extension "org"
          :auto-sitemap t
@@ -14,9 +14,15 @@
          :makeindex t
          :html-doctype "html5"
          :html-html5-fancy t
-                                        ; Disable some Org's HTML defaults
          :html-head-include-scripts nil
          :html-head-include-default-style nil
-         :html-head "<link rel=\"stylesheet\" href=\"https:\/\/cdn.jsdelivr.net\/npm\/bulma@0.9.2\/css\/bulma.min.css\">"
+         :html-head "<link rel=\"stylesheet\" href=\"css/org.css\">"
          :publishing-directory "./docs"
-         :publishing-function org-html-publish-to-html)))
+         :publishing-function org-html-publish-to-html)
+        ("wiki-static"
+         :base-directory "."
+         :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
+         :publishing-directory "./docs"
+         :recursive t
+         :publishing-function org-publish-attachment)
+        ("wiki" :components ("wiki-content" "wiki-static"))))
