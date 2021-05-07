b48149244a00d48054f32bb9dba7210eb9015c4d
Joel Holmes <holmes89@gmail.com>
2021-05-07 12:16:02 -0400

    formatting

--- build/publish-doc.el
+++ build/publish-doc.el
@@ -12,5 +12,11 @@
          :sitemap-title "Personal Wiki"
          :recursive t
          :makeindex t
+         :html-doctype "html5"
+         :html-html5-fancy t
+                                        ; Disable some Org's HTML defaults
+         :html-head-include-scripts nil
+         :html-head-include-default-style nil
+         :html-head "<link rel=\"stylesheet\" href=\"https:\/\/cdn.jsdelivr.net\/npm\/bulma@0.9.2\/css\/bulma.min.css\">"
          :publishing-directory "./docs"
          :publishing-function org-html-publish-to-html)))
